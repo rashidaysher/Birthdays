4818c621048816a83200b74b1b63a2a9d5110150
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-07-20 21:10:38 +0300

    intergrating the from

--- js/app.js
+++ js/app.js
@@ -2,3 +2,6 @@
 let month =["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",];
 
 // weird male names
+const maleAkan = [ "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame", 
+
+]
